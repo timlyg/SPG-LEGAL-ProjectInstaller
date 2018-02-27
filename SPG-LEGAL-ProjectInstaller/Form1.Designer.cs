8ba00cd6b011844c7a19c1c94bf90e4284fb7ed5
Timothy Law <timlyg@yahoo.com>
2018-02-27 17:39:20 -0500

    First Batch

---
+++ SPG-LEGAL-ProjectInstaller/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace SPG_LEGAL_ProjectInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.getVMWare = new System.Windows.Forms.CheckBox();
+            this.getCitrix = new System.Windows.Forms.CheckBox();
+            this.getRelativity = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.getClearOutlook = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.getTeamViewer = new System.Windows.Forms.CheckBox();
+            this.getCitrixWebPlugin = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download + Install Checked Items";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // getVMWare
+            // 
+            this.getVMWare.AutoSize = true;
+            this.getVMWare.Location = new System.Drawing.Point(23, 35);
+            this.getVMWare.Name = "getVMWare";
+            this.getVMWare.Size = new System.Drawing.Size(170, 17);
+            this.getVMWare.TabIndex = 2;
+            this.getVMWare.Text = "Download and Install VMWare";
+            this.getVMWare.UseVisualStyleBackColor = true;
+            // 
+            // getCitrix
+            // 
+            this.getCitrix.AutoSize = true;
+            this.getCitrix.Location = new System.Drawing.Point(23, 59);
+            this.getCitrix.Name = "getCitrix";
+            this.getCitrix.Size = new System.Drawing.Size(214, 17);
+            this.getCitrix.TabIndex = 3;
+            this.getCitrix.Text = "Download and Install Citrix Receiver 4.5";
+            this.getCitrix.UseVisualStyleBackColor = true;
+            // 
+            // getRelativity
+            // 
+            this.getRelativity.AutoSize = true;
+            this.getRelativity.Location = new System.Drawing.Point(23, 83);
+            this.getRelativity.Name = "getRelativity";
+            this.getRelativity.Size = new System.Drawing.Size(235, 17);
+            this.getRelativity.TabIndex = 4;
+            this.getRelativity.Text = "Download and Install Relativity Receiver 4.9";
+            this.getRelativity.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(530, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 391);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(530, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // getClearOutlook
+            // 
+            this.getClearOutlook.AutoSize = true;
+            this.getClearOutlook.Checked = true;
+            this.getClearOutlook.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.getClearOutlook.Location = new System.Drawing.Point(23, 133);
+            this.getClearOutlook.Name = "getClearOutlook";
+            this.getClearOutlook.Size = new System.Drawing.Size(213, 17);
+            this.getClearOutlook.TabIndex = 8;
+            this.getClearOutlook.Text = "Download and Install Clear_Outlook.bat";
+            this.getClearOutlook.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(249, 214);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // getTeamViewer
+            // 
+            this.getTeamViewer.AutoSize = true;
+            this.getTeamViewer.Location = new System.Drawing.Point(23, 156);
+            this.getTeamViewer.Name = "getTeamViewer";
+            this.getTeamViewer.Size = new System.Drawing.Size(187, 17);
+            this.getTeamViewer.TabIndex = 10;
+            this.getTeamViewer.Text = "Download and Install TeamViewer";
+            this.getTeamViewer.UseVisualStyleBackColor = true;
+            // 
+            // getCitrixWebPlugin
+            // 
+            this.getCitrixWebPlugin.AutoSize = true;
+            this.getCitrixWebPlugin.Location = new System.Drawing.Point(23, 106);
+            this.getCitrixWebPlugin.Name = "getCitrixWebPlugin";
+            this.getCitrixWebPlugin.Size = new System.Drawing.Size(205, 17);
+            this.getCitrixWebPlugin.TabIndex = 11;
+            this.getCitrixWebPlugin.Text = "Download and Install Citrix WebPlugin";
+            this.getCitrixWebPlugin.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(20, 263);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "CleanUp";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 413);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.getCitrixWebPlugin);
+            this.Controls.Add(this.getTeamViewer);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.getClearOutlook);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.getRelativity);
+            this.Controls.Add(this.getCitrix);
+            this.Controls.Add(this.getVMWare);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SPG-LEGAL Batch Installer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox getVMWare;
+        private System.Windows.Forms.CheckBox getCitrix;
+        private System.Windows.Forms.CheckBox getRelativity;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.CheckBox getClearOutlook;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox getTeamViewer;
+        private System.Windows.Forms.CheckBox getCitrixWebPlugin;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SPG-LEGAL-ProjectInstaller/Form1.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SPG_LEGAL_ProjectInstaller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Temporary section
+            // Get all current running processes into Combo Box1
+            comboBox1.Items.Clear();
+            Process[] MyProcess = Process.GetProcesses();
+            for (int i = 0; i < MyProcess.Length; i++)
+                comboBox1.Items.Add(MyProcess[i].ProcessName);
+            // comboBox1.Items.Add(MyProcess[i].ProcessName + "-" + MyProcess[i].Id);
+            comboBox1.Sorted = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Ready";
+        }
+
+        // VMWare Installation
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string adminname = "SPGadmin";
+            string adminpass = "";
+            toolStripStatusLabel1.Text = "Analyzing Installed packages...";
+
+            // Verify checked installation & if they had already been installed
+            if (getVMWare.Checked == true || getCitrix.Checked == true || getRelativity.Checked == true)
+            {
+                Process myProcess = new Process();
+
+                myProcess.StartInfo.UseShellExecute = false;
+                myProcess.StartInfo.FileName = "wmic.exe";
+                myProcess.StartInfo.Arguments = "product list brief";
+                // myProcess.StartInfo.CreateNoWindow = true;
+                myProcess.StartInfo.RedirectStandardOutput = true;
+                myProcess.Start();
+
+                string standard_output;
+                while ((standard_output = myProcess.StandardOutput.ReadLine()) != null)
+                {
+                    if (standard_output.Contains("VMware Horizon Client"))
+                    {
+                        //MessageBox.Show("VMWare had already been installed");
+                        getVMWare.Checked = false;
+                    }
+                    if (standard_output.Contains("Citrix Receiver Inside"))
+                    {
+                        //MessageBox.Show("Citrix had already been installed");
+                        getCitrix.Checked = false;
+                    }
+                    if (standard_output.Contains("Relativity Web Client"))
+                    {
+                        //MessageBox.Show("Relativity had already been installed");
+                        getRelativity.Checked = false;
+                    }
+                }
+
+                myProcess.WaitForExit();
+
+            }
+            // Handle VMWare installation
+            if (getVMWare.Checked == true)
+            {
+                string fileName = "VMware-Horizon-View-Client-x86_64-3.5.2-3150477.exe";
+                string remoteUri = "http://nycphantom.com/upload/uploaded/" + fileName;
+                // Create a new WebClient instance.
+                WebClient myWebClient = new WebClient();
+                toolStripStatusLabel1.Text = "Downloading File " + fileName + " from " + remoteUri + ".......";
+                // Download the Web resource and save it into the current filesystem folder.
+                myWebClient.DownloadFile(remoteUri, @"C:\Users\Public\Downloads\" + fileName);
+                toolStripStatusLabel1.Text = "Successfully Downloaded File " + fileName + " from " + remoteUri;
+                toolStripStatusLabel1.Text = "Downloaded file saved in the following file system folder: " + "C:\\Users\\Public\\Downloads\\";
+
+                // Install VMWare in silent mode
+                var psi = new ProcessStartInfo();
+
+                psi.UserName = adminname;
+                psi.Password = new NetworkCredential("", adminpass).SecurePassword;
+                psi.UseShellExecute = false;
+                psi.WorkingDirectory = @"C:\Users\Public\Downloads\";
+                psi.FileName = @"C:\Users\Public\Downloads\" + fileName;
+                psi.Arguments = "/s /v\" /qn REBOOT=Reallysuppress\"";
+                Process.Start(psi);
+            }
+
+
+            // Handle Citrix installation
+            if (getCitrix.Checked == true)
+            {
+                string fileName = "CitrixReceiver-4.5.exe";
+                string remoteUri = "http://nycphantom.com/upload/uploaded/" + fileName;
+                // Create a new WebClient instance.
+                WebClient myWebClient = new WebClient();
+                toolStripStatusLabel1.Text = "Downloading File " + fileName + " from " + remoteUri + ".......";
+                // Download the Web resource and save it into the current filesystem folder.
+                myWebClient.DownloadFile(remoteUri, @"C:\Users\Public\Downloads\" + fileName);
+                toolStripStatusLabel1.Text = "Successfully Downloaded File " + fileName + " from " + remoteUri;
+                toolStripStatusLabel1.Text = "Downloaded file saved in the following file system folder: " + "C:\\Users\\Public\\Downloads\\";
+
+                // Install Citrix 4.5 in silent mode
+                var psi = new ProcessStartInfo();
+                // Citrix installation doesn't require admin elevation
+                //psi.UserName = "administrator";
+                //psi.Password = new NetworkCredential("", "password").SecurePassword;
+                //psi.UseShellExecute = false;
+                psi.WorkingDirectory = @"C:\Users\Public\Downloads\";
+                psi.FileName = @"C:\Users\Public\Downloads\" + fileName;
+                psi.Arguments = "/silent";
+                Process.Start(psi);
+            }
+
+            // Handle Relativity installation
+            if (getRelativity.Checked == true)
+            {
+                string fileName = "ViewerInstallationKit9.4.496.3.exe";
+                string remoteUri = "http://nycphantom.com/upload/uploaded/" + fileName;
+                // Create a new WebClient instance.
+                WebClient myWebClient = new WebClient();
+                toolStripStatusLabel1.Text = "Downloading File " + fileName + " from " + remoteUri + ".......";
+                // Download the Web resource and save it into the current filesystem folder.
+                myWebClient.DownloadFile(remoteUri, @"C:\Users\Public\Downloads\" + fileName);
+                toolStripStatusLabel1.Text = "Successfully Downloaded File " + fileName + " from " + remoteUri;
+                toolStripStatusLabel1.Text = "Downloaded file saved in the following file system folder: " + "C:\\Users\\Public\\Downloads\\";
+
+                // Install Relativity in silent mode
+                var psi = new ProcessStartInfo();
+                //psi.UserName = adminname;
+                //psi.Password = new NetworkCredential("", adminpass).SecurePassword;
+                //psi.UseShellExecute = false;
+                psi.UseShellExecute = true;
+                psi.WorkingDirectory = @"C:\Windows\System32";
+                //psi.WorkingDirectory = @"C:\Users\Public\Downloads\";
+                psi.FileName = @"C:\Users\Public\Downloads\" + fileName;
+                psi.Arguments = "/silent";
+                psi.Verb = "runas";
+                Process.Start(psi);
+            }
+
+
+
+
+            // Handle Citrix WebPlugin installation
+            if (getCitrixWebPlugin.Checked == true)
+            {
+                string fileName = "CitrixOnlinePluginWeb.exe";
+                string remoteUri = "http://nycphantom.com/upload/uploaded/" + fileName;
+                // Create a new WebClient instance.
+                WebClient myWebClient = new WebClient();
+                toolStripStatusLabel1.Text = "Downloading File " + fileName + " from " + remoteUri + ".......";
+                // Download the Web resource and save it into the current filesystem folder.
+                myWebClient.DownloadFile(remoteUri, @"C:\Users\Public\Downloads\" + fileName);
+                toolStripStatusLabel1.Text = "Successfully Downloaded File " + fileName + " from " + remoteUri;
+                toolStripStatusLabel1.Text = "Downloaded file saved in the following file system folder: " + "C:\\Users\\Public\\Downloads\\";
+
+                // Install Relativity in silent mode
+                var psi = new ProcessStartInfo();
+                //psi.UserName = adminname;
+                //psi.Password = new NetworkCredential("", adminpass).SecurePassword;
+                //psi.UseShellExecute = false;
+                psi.UseShellExecute = true;
+                psi.WorkingDirectory = @"C:\Windows\System32";
+                //psi.WorkingDirectory = @"C:\Users\Public\Downloads\";
+                psi.FileName = @"C:\Users\Public\Downloads\" + fileName;
+                psi.Arguments = "/silent";
+                psi.Verb = "runas";
+                Process.Start(psi);
+            }
+
+
+
+
+            // Handle Clear_Outook
+            if (getClearOutlook.Checked == true)
+            {
+                string fileName = "clear_outlook.bat";
+                string remoteUri = "http://nycphantom.com/upload/uploaded/" + fileName;
+                // Create a new WebClient instance.
+                WebClient myWebClient = new WebClient();
+                toolStripStatusLabel1.Text = "Downloading File " + fileName + " from " + remoteUri + ".......";
+                // Download the Web resource and save it into the current filesystem folder.
+                myWebClient.DownloadFile(remoteUri, @"C:\Users\Public\Downloads\" + fileName);
+                toolStripStatusLabel1.Text = "Successfully Downloaded File " + fileName + " from " + remoteUri;
+                toolStripStatusLabel1.Text = "Downloaded file saved in the following file system folder: " + "C:\\Users\\Public\\Downloads\\";
+
+                // Run Clear_Outlook batch file
+                var psi = new ProcessStartInfo();
+                // Citrix installation doesn't require admin elevation
+                //psi.UserName = "administrator";
+                //psi.Password = new NetworkCredential("", "password").SecurePassword;
+                //psi.UseShellExecute = false;
+                psi.WorkingDirectory = @"C:\Users\Public\Downloads\";
+                psi.FileName = @"C:\Users\Public\Downloads\" + fileName;
+                Process.Start(psi);
+            }
+
+
+
+
+            // Handle TeamViewer with Admin rights installation
+            if (getTeamViewer.Checked == true)
+            {
+                string fileName = "TeamViewer_Setup.exe";
+                string remoteUri = "http://nycphantom.com/upload/uploaded/" + fileName;
+                // Create a new WebClient instance.
+                WebClient myWebClient = new WebClient();
+                toolStripStatusLabel1.Text = "Downloading File " + fileName + " from " + remoteUri + ".......";
+                // Download the Web resource and save it into the current filesystem folder.
+                myWebClient.DownloadFile(remoteUri, @"C:\Users\Public\Downloads\" + fileName);
+                toolStripStatusLabel1.Text = "Downloaded file saved in the following file system folder: " + "C:\\Users\\Public\\Downloads\\";
+
+                // Install VMWare in silent mode
+                var psi = new ProcessStartInfo();
+
+                psi.UserName = adminname;
+                psi.Password = new NetworkCredential("", adminpass).SecurePassword;
+                psi.UseShellExecute = false;
+                psi.WorkingDirectory = @"C:\Users\Public\Downloads\";
+                psi.FileName = @"C:\Users\Public\Downloads\" + fileName;
+                Process.Start(psi);
+                // "directory name invalid" error is referring to the SPGadmin user not able to access LAUser folder, this sort of thing. If this is an issue, work with file in Users/Public then.
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process process in processlist)
+            {
+                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    Console.WriteLine("Process: {0} ID: {1} Window title: {2}", process.ProcessName, process.Id, process.MainWindowTitle);
+                }
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string[] desktopFiles =     // Desktop Folder files to remain
+            {
+                "desktop.ini",
+                "VMware Horizon Client.lnk",
+                "Google Chrome.lnk",
+                "GoToAssist Customer.lnk",
+                "Outlook 2016.lnk",
+                "Relativity.url",
+                "SPG-LEGAL-ProjectInstaller.exe",
+                "Recycle Bin"
+            };
+            string[] documentsFiles =     // My Docs Folder files to remain
+            {
+                "SPG-LEGAL-ProjectInstaller.exe",
+                "Custom Office Templates",
+                "OneNote Notebooks",
+                "Sound recordings"
+            };
+            string[] downloadsFiles =     // Downloads Folder files to remain
+            {
+                "CitrixOnlinePluginWeb.exe",
+                "ViewerInstallationKit.exe",
+                "VMware-Horizon-View-Client-x86_64-3.5.2-3150477.exe",
+                "Setup.X86.en-us_O365BusinessRetail_0ac07f2e-0865-40af-bf1b-7077409aecd4_TX_PR_b_32_.exe",
+                "SPG-LEGAL-ProjectInstaller.exe",
+                "CutePDF Writer",
+                "Citrix Receiver",
+                "Citrix Receiver4.7",
+                "Citrix Receiver 4.7",
+                "CitrixReceiver4.5",
+                "CitrixReceiver 4.5",
+                "GoToAssist Installer 3.1.0.1251",
+                "GoToAssist 3_5 1488",
+                "My Company_Default Group_WIN64BIT",
+                "Reboot Restore Rx 20160810 v2.1"
+            };
+
+            string userProfile = Environment.GetEnvironmentVariable("USERPROFILE");
+
+            if (!userProfile.Contains("LAuser") && !userProfile.Contains("NYuser"))
+            {
+                MessageBox.Show(userProfile + ": User Profile Cleaning not allowed");
+                return;
+            }
+
+
+
+
+            string desktopPath = Path.Combine(userProfile, "Desktop");
+            string documentsPath = Path.Combine(userProfile, "Documents");
+            string downloadsPath = Path.Combine(userProfile, "Downloads");
+
+            // Start with Desktop cleaning
+            string[] filePaths = Directory.GetFiles(desktopPath);
+            foreach (string filePath in filePaths)
+            {
+                if (desktopFiles.Contains(Path.GetFileName(filePath)))
+                {
+                   // keep these files
+                }
+                else
+                {
+                    File.Delete(filePath);
+                }
+            }
+            filePaths = Directory.GetDirectories(desktopPath);
+            foreach (string filePath in filePaths)
+            {
+                if (desktopFiles.Contains(Path.GetFileName(filePath)))
+                {
+                    // keep these files
+                }
+                else
+                {
+                    Directory.Delete(filePath,true);
+                }
+            }
+
+
+            // Start with Documents cleaning
+            filePaths = Directory.GetFiles(documentsPath);
+            foreach (string filePath in filePaths)
+            {
+                if (documentsFiles.Contains(Path.GetFileName(filePath)))
+                {
+                    // keep these files
+                }
+                else
+                {
+                    File.Delete(filePath);
+                }
+            }
+            filePaths = Directory.GetDirectories(documentsPath);
+            foreach (string filePath in filePaths)
+            {
+                if (documentsFiles.Contains(Path.GetFileName(filePath)))
+                {
+                    // keep these files
+                }
+                else
+                {
+                    Directory.Delete(filePath, true);
+                }
+            }
+
+
+            // Start with Downloads cleaning
+            filePaths = Directory.GetFiles(downloadsPath);
+            foreach (string filePath in filePaths)
+            {
+                if (downloadsFiles.Contains(Path.GetFileName(filePath)))
+                {
+                    // keep these files
+                }
+                else
+                {
+                    File.Delete(filePath);
+                }
+            }
+            filePaths = Directory.GetDirectories(downloadsPath);
+            foreach (string filePath in filePaths)
+            {
+                if (downloadsFiles.Contains(Path.GetFileName(filePath)))
+                {
+                    // keep these files
+                }
+                else
+                {
+                    Directory.Delete(filePath, true);
+                }
+            }
+
+
+        }
+    }
+}
